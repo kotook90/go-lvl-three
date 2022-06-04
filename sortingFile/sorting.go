2499282609a6bbead70b5ead634a9fa93bd1fe77
kotook90 <99076387+kotook90@users.noreply.github.com>
2022-06-04 15:12:42 +0300

    Update sorting.go

--- sortingFile/sorting.go
+++ sortingFile/sorting.go
@@ -14,7 +14,7 @@ type CopyFiles struct {
 
 var LetFile []CopyFiles
 
-func Sorting(infoOfAllFiles map[string]os.FileInfo) (InfoOfCopy []CopyFiles, err error) {
+func Sorting(infoOfAllFiles map[string]os.FileInfo) (infoOfCopy []CopyFiles, err error) {
 
 	//тут я перезаписываю все имена, размеры и пути найденных файлов из мапы в слайс. Ключ мапы это путь к файлу(string)
 
@@ -22,14 +22,14 @@ func Sorting(infoOfAllFiles map[string]os.FileInfo) (InfoOfCopy []CopyFiles, err
 		LetFile = append(LetFile, CopyFiles{v.Name(), v.Size(), k}) //letFile это слайс, который содержит имя, размер и путь всех найденных файлов
 
 	}
-	InfoOfCopy = LetFile
+	infoOfCopy = LetFile
 	//fmt.Println(InfoOfCopy)
 
-	return InfoOfCopy, nil
+	return infoOfCopy, nil
 }
 
 // следующий лог
-func DuplicateList(InfoOfCopy []CopyFiles) (duplicateAmount map[CopyFiles]int) {
+func DuplicateList(infoOfCopy []CopyFiles) (duplicateAmount map[CopyFiles]int) {
 
 	log.SetFormatter(&log.JSONFormatter{})
 	standartFields := log.Fields{
@@ -39,11 +39,11 @@ func DuplicateList(InfoOfCopy []CopyFiles) (duplicateAmount map[CopyFiles]int) {
 
 	//var copyList2 []CopyFiles
 	duplicateAmount = make(map[CopyFiles]int)
-	for i := 0; i < len(InfoOfCopy); i++ {
-		for j := i + 1; j < len(InfoOfCopy); j++ {
-			if InfoOfCopy[i].Name == InfoOfCopy[j].Name && InfoOfCopy[i].Size == InfoOfCopy[j].Size {
+	for i := 0; i < len(infoOfCopy); i++ {
+		for j := i + 1; j < len(infoOfCopy); j++ {
+			if infoOfCopy[i].Name == infoOfCopy[j].Name && infoOfCopy[i].Size == infoOfCopy[j].Size {
 
-				duplicateAmount[InfoOfCopy[j]]++
+				duplicateAmount[infoOfCopy[j]]++
 			}
 		}
 
